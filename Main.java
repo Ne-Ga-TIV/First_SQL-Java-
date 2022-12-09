3f21d32171a10f78c9b1f83d7da6edf6df652513
Ne-Ga-TIV <anton.antonio.10@mail.ru>
2022-12-09 12:26:41 +0200

    Add connection to DB  tools

--- Main.java
+++ Main.java
@@ -3,7 +3,57 @@ import java.sql.*;
 
 public class Main{
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world\n");
+    private final static String userName = "anva8129";
+    private final static String password = "cacaite228";
+
+
+    public static void loadDriver()
+    {
+       try {
+         Class.forName("org.postgresql.Driver");
+       }
+       catch (ClassNotFoundException cnfe) {
+         System.out.println("Couldn't find driver class!");
+         cnfe.printStackTrace();
+         System.exit(1);
+       }
+    }
+
+    public static Connection getConnection() 
+    {
+        Connection postGresConn = null;
+        try {
+          postGresConn = DriverManager.getConnection("jdbc:postgresql://pgsql3.mif/studentu", userName, password);
+        }
+        catch (SQLException sqle) {
+          System.out.println("Couldn't connect to database!");
+          sqle.printStackTrace();
+          return null ;
+        }
+        return postGresConn ;
+     }
+
+
+    public static void closeConnection(Connection con)
+    {
+        try{
+            con.close();
+        }
+        catch(SQLException sqle){
+            System.out.println("Couldn`t close connect to database!");
+            sqle.printStackTrace();
+            System.exit(2);
+        }
+    } 
+    public static void main(String[] args)
+     {
+        loadDriver();
+        Connection connection  = getConnection();
+        
+
+
+
+        closeConnection(connection);
+        
     }
 }
