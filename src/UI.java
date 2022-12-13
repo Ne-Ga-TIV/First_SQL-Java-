76b64d37e71ee9f374da38177d6dbb797a1fba04
Ne-Ga-TIV <anton.antonio.10@mail.ru>
2022-12-13 10:48:43 +0200

    Fix

--- src/UI.java
+++ src/UI.java
@@ -27,12 +27,12 @@ public class UI {
     public static void printTrips(ResultSet trip){
         int i = 1;
         try{
-            Runtime.getRuntime().exec("clear");
+            Runtime.getRuntime().exec("cls");
         }catch(Exception e){}
         System.out.printf("NR     %20s %20s %20s %20s %20s\n", "FROR", "TO", "DATE START", "DATE END", "COMPANY");
         try{
             while(trip.next()){
-                System.out.printf("%-7d %20s %20s %20s %20s %20s\n", i, trip.getString(1), trip.getString(2), trip.getDate(3).toString(),
+                System.out.printf("[%-7d] %20s %20s %20s %20s %20s\n", i, trip.getString(1), trip.getString(2), trip.getDate(3).toString(),
                                     trip.getDate(4).toString(), trip.getString(5));
                 i++;
             }
